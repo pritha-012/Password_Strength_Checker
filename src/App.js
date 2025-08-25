d8f7753d3dad0fe2d216797361ff62d42bf29ccb
Pritha Das <prithadas976@gmail.com>
2025-08-26 00:05:49 +0530

    initial commit of this proj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [password, setPassword] = useState('');
+  const [strength, setStrength] = useState('');
+
+  const checkStrength = (pwd) => {
+    let strength = '';
+    const hasUpper = /[A-Z]/.test(pwd);
+    const hasNumber = /[0-9]/.test(pwd);
+    const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(pwd);
+
+    if (pwd.length < 6) {
+      strength = 'Weak';
+    } else if (pwd.length >= 6 && (hasUpper || hasNumber || hasSymbol)) {
+      strength = 'Medium';
+    } 
+    if (pwd.length >= 8 && hasUpper && hasNumber && hasSymbol) {
+      strength = 'Strong';
+    }
+
+    return strength;
+  };
+
+  const handleChange = (e) => {
+    const pwd = e.target.value;
+    setPassword(pwd);
+    setStrength(checkStrength(pwd));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <h1>Password Strength Checker</h1>
+      <input 
+        type="password" 
+        placeholder="Enter password" 
+        value={password}
+        onChange={handleChange}
+      />
+      {password && (
+        <p className={`strength ${strength.toLowerCase()}`}>
+          Strength: {strength}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
